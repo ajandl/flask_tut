53ed87e219d2b0bff132753f203239157612ece6
Adam Jandl <jandl.wisc@gmail.com>
2022-05-05 22:15:41 -0400

    Templates with loops and inheritance

--- app1/__init__.py
+++ app1/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
 
 app = Flask(__name__)
+'''creates an instance of Flask, many optional parameters are omitted here.
+One optional parameter omitted above is template_folder.
+This defaults to templates'''
 
 from app1 import routes
--- app1/routes.py
+++ app1/routes.py
@@ -6,10 +6,20 @@ from app1 import app
 @app.route('/index')
 def index():
     user = {'username': 'Adam2'}
+    posts = [
+        {
+            'author': {'username': 'User1'},
+            'body': 'User1 Post1'
+        },
+        {
+            'author': {'username': 'User2'},
+            'body': 'User2 Post1'
+        }
+    ]
 
     ''' render template knows to look in the templates folder based on the
     optional omitted parameter when creating the flask instance in __init__'''
-    return render_template('index.html', title='Home', user=user)
+    return render_template('index.html', title='Home', user=user, posds=posts)
 
 
 @app.route('/data')
